a3f5034b6c19bbf8bfa5570a7de557887d67ccf6
shivamakhauri04 <shivamakhauri04@gmail.com>
2019-11-29 22:11:53 -0500

    [2.4]Added Doxygen comments to to the header files.Added License preamble to each .cpp and header files

--- include/Navigation.hpp
+++ include/Navigation.hpp
@@ -1,3 +1,44 @@
+/**
+BSD 3-Clause License
+
+Copyright (c) 2019, Shivam Akhauri,Toyas Dhake
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
+
+/**
+* @file Navigation.hpp
+* @author Shivam Akhauri, Toyas Dhake
+* @date 27 November 2019
+* @copyright BSD 3-clause, 2019 Shivam Akhauri,Toyas Dhake
+* @brief Header for the robot autonomous of the robot
+*/
+
 #ifndef INCLUDE_NAVIGATION_HPP_
 #define INCLUDE_NAVIGATION_HPP_
 
@@ -11,6 +52,11 @@
 #include "TurtlebotStates.hpp"
 #include "QLearning.hpp"
  
+/**
+* @brief Class Navigation
+* class variables to subscribe to odometry
+* publish velocity
+*/
 class Navigation {
  private:
     QLearning qLearning;
@@ -23,15 +69,82 @@ class Navigation {
     bool isCollision;
 
  public:
+    /**
+    * @brief constructor Navigation class
+    * @param none
+    * @return none
+    * initializes the publisher and subsciber
+    * initialize the value of odometry
+    * initialize the liner and angular speed
+    */
     Navigation();
+    /**
+    * @brief destructor Navigation class
+    * @param none
+    * @return none
+    * Destructor for the navigation clas
+    */
     ~Navigation();
+    /**
+    * @brief function trainRobot
+    * @param path std::string
+    * @return none
+    * training of the agent by receiving states
+    * perform actions in that states and receive rewards
+    */
     void trainRobot(std::string path);
+    /**
+    * @brief function testRobot
+    * @param path std::string
+    * @return none
+    * Runs the inferece code 
+    * the bot uses the trained model to navigate
+    */
     void testRobot(std::string path);
+    /**
+    * @brief function environmentReset
+    * @param void
+    * @return void
+    * resets the states and the environment
+    */
     void environmentReset();
+     /**
+    * @brief function environmentPause
+    * @param none
+    * @return none
+    * pauses the gazebo environment
+    */
     void environmentPause();
+    /**
+    * @brief function environmentUnpause
+    * @param none
+    * @return none
+    * unpauses the gazebo environment
+    */
     void environmentUnpause();
+    /**
+    * @brief function action
+    * @param int action
+    * @param bool colStatus 
+    * @param int reward 
+    * @param int nextState 
+    * @return none
+    * determines the robot actions based on states and RL
+    */
     void action(int action, bool &colStatus, int &reward, int &nextState);
+    /**
+    * @brief function demoAction
+    * @param int action
+    * @return none
+    * publishes linear and angular velocities to the turtlebot
+    */
     void demoAction(int action);
+    /**
+    * @brief function demoAction
+    * @param std::vector<int> state
+    * @return int stateIndex
+    * mapping the vector to the state in rl table
+    */
     int getStateIndex(std::vector<int> state);
 };
 
